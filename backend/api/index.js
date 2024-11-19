cdb5b2ab9aaf25f6a336ec2c49b62669d159c4e7
Guru20154 <gurkanwal04singh@gmail.com>
2024-11-19 16:12:56 +0530

    backend veercel

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,6 @@
-import connectDB from "./db/index.js";
+import connectDB from "../src/db/index.js";
 import dotenv from "dotenv";
-import { app } from "./app.js";
+import { app } from "../src/app.js";
 
 dotenv.config({
     path: './.env'
